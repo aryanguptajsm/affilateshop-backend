ffd22224cddc52b48ea3946c1dd31780befb395b
Aryan Gupta <aryangupta.jsm@gmail.com>
2025-11-14 15:43:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/products", (req, res) => {
   res.json(products);
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
